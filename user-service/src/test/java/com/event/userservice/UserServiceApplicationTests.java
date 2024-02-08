beb6e8d8290ef1bad689b484c6a88b76f46a3e7b
Asulan Maksut <a.maksut@strongte.am>
2024-02-08 12:32:10 +0600

    init user-service

---
+++ user-service/src/test/java/com/event/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
